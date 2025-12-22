efcbd2d09e2b7ef9780b368d5cc24f15dbecae55
aaaloo <anismarzuki03@gmail.com>
2025-12-22 11:49:18 +0800

    Update eclipse project configuration and source files

--- eclipse-workspace/ParallelProject/src/RandomNumberThread.java
+++ eclipse-workspace/ParallelProject/src/RandomNumberThread.java
@@ -1,4 +1,27 @@
-
+import java.util.*;
 public class RandomNumberThread {
 
+	static final List<Integer> randomNumbers = new ArrayList<>();
+	static final Object lock = new Object();
+    static boolean randomReady = false;
+
+	// Thread 1: Generate Random Numbers
+        public void run() {
+            synchronized (lock) {
+                Random rand = new Random();
+                System.out.println("\n[T1] Generating random numbers:");
+                for (int i = 0; i < 10; i++) {
+                    int num = rand.nextInt(100);
+                    randomNumbers.add(num);
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+
+                // Notify others
+                randomReady = true;
+                lock.notifyAll();
+            }
+        
+        
+}
 }
